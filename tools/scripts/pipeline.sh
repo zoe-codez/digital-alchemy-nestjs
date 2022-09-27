b2b90dbbceb291b8f3091827a881f167f655ed0d
mp3three <git@mp3three.dev>
2022-09-27 17:38:42 -0500

    build

--- tools/scripts/pipeline.sh
+++ tools/scripts/pipeline.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 npx nx run-many --target=test --all || exit 1
 npx nx run-many --target=build --all || exit 1
-node ./pipeline.js
+node tools/scripts/pipeline.js
 npx nx run-many --target=publish --all
