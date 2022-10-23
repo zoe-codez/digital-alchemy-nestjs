14fd7240eb657dd7784a5dd964e1de2c1d54603f
mp3three <git@mp3three.dev>
2022-10-23 11:26:09 -0500

    WIP pipeline

--- apps/build-pipeline/src/main.ts
+++ apps/build-pipeline/src/main.ts
@@ -1,24 +1,44 @@
+/* eslint-disable radar/no-duplicate-string */
 import { QuickScript } from "@steggy/boilerplate";
 import JSON from "comment-json";
 import dayjs from "dayjs";
 import { readFileSync, writeFileSync } from "fs";
+import { join } from "path";
 import { inc } from "semver";
 
 @QuickScript()
 export class BuildPipelineService {
   public exec(): void {
-    const packageJSON = JSON.parse(
+    const rootPackage = JSON.parse(
       readFileSync("package.json", "utf8"),
     ) as unknown as {
       version: string;
     };
     const prefix = dayjs().format("YY.ww");
-    packageJSON.version = packageJSON.version.startsWith(prefix)
-      ? inc(packageJSON.version, "patch")
+    rootPackage.version = rootPackage.version.startsWith(prefix)
+      ? inc(rootPackage.version, "patch")
       : `${prefix}.0`;
     writeFileSync(
       "package.json",
-      JSON.stringify(packageJSON, undefined, "  ") + `\n`,
+      JSON.stringify(rootPackage, undefined, "  ") + `\n`,
     );
+
+    const workspace = JSON.parse(
+      readFileSync("workspace.json", "utf8"),
+    ) as unknown as {
+      projects: Record<string, string>;
+    };
+    Object.values(workspace.projects).forEach(base => {
+      const packageJSON = JSON.parse(
+        readFileSync(join(base, "package.json"), "utf8"),
+      ) as unknown as {
+        version: string;
+      };
+      packageJSON.version = rootPackage.version;
+      writeFileSync(
+        join(base, "package.json"),
+        JSON.stringify(packageJSON, undefined, "  ") + `\n`,
+      );
+    });
   }
 }
--- tools/scripts/pipeline.sh
+++ tools/scripts/pipeline.sh
@@ -2,4 +2,5 @@
 npx nx run-many --target=test --all || exit 1
 npx nx run-many --target=build --all --configuration=production || exit 1
 node tools/scripts/pipeline.js
+npx nx run-many --target=build --all --configuration=production || exit 1
 npx nx run-many --target=publish --all
