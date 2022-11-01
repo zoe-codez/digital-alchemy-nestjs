b8af73683b2d5d00690bb25178401d36825fa896
mp3three <git@mp3three.dev>
2022-10-31 23:01:39 -0500

    improvements to menu sampler

--- apps/sampler-app/src/services/menu-sampler.service.ts
+++ apps/sampler-app/src/services/menu-sampler.service.ts
@@ -4,6 +4,9 @@ import { Injectable } from "@nestjs/common";
 import {
   ApplicationManagerService,
   ColorsService,
+  FontAwesomeIcons,
+  GV,
+  MainMenuCB,
   MainMenuEntry,
   MenuComponentOptions,
   PromptService,
@@ -18,6 +21,7 @@ type tMenuOptions = MenuComponentOptions & {
   generateCount: number;
 } & Record<"optionsLeft" | "optionsRight", FakerSources>;
 const CHUNKY_LIST = 50;
+const MINI_LIST = 10;
 
 /**
  * Just a few items to make life interesting
@@ -32,8 +36,42 @@ enum FakerSources {
   product = "product",
 }
 
+const PRE_GENERATED_MENU: MainMenuEntry<string>[] = [];
+
+PEAT(MINI_LIST).forEach(() =>
+  PRE_GENERATED_MENU.push({
+    entry: [faker.random.word()],
+    type: "Word",
+  }),
+);
+PEAT(MINI_LIST).forEach(() =>
+  PRE_GENERATED_MENU.push({
+    entry: [faker.system.filePath()],
+    type: "File Path",
+  }),
+);
+PEAT(MINI_LIST).forEach(() =>
+  PRE_GENERATED_MENU.push({
+    entry: [faker.vehicle.vin()],
+    type: "VIN",
+  }),
+);
+PEAT(MINI_LIST).forEach(() =>
+  PRE_GENERATED_MENU.push({
+    entry: [faker.commerce.productName()],
+    type: "Product Name",
+  }),
+);
+PEAT(MINI_LIST).forEach(() =>
+  PRE_GENERATED_MENU.push({
+    entry: [faker.address.streetAddress()],
+    type: "Street Address",
+  }),
+);
+
 type tAsyncExample = { color?: string; song?: string };
 const DEFAULT_GENERATE = 50;
+type AdvancedMenuResult = string | { type: string };
 
 @Injectable()
 export class MenuSampler {
@@ -45,6 +83,7 @@ export class MenuSampler {
     private readonly text: TextRenderingService,
   ) {}
 
+  private hiddenTypes: string[] = [];
   private menuOptions: tMenuOptions = {
     condensed: false,
     generateCount: DEFAULT_GENERATE,
@@ -58,22 +97,41 @@ export class MenuSampler {
     showHeaders: true,
     showHelp: true,
   };
+  private storedValues: string[] = [];
 
-  public async exec(): Promise<void> {
+  public async exec(value?: string): Promise<void> {
     this.application.setHeader("Menu Sampler");
     const action = await this.prompt.menu({
+      condensed: true,
+      hideSearch: true,
       keyMap: {
-        a: ["async"],
-        b: {
-          entry: ["basic"],
-          highlight: {
-            normal: chalk.green.dim,
-            valueMatch: chalk.green.bold,
-          },
+        a: {
+          entry: ["async"],
+          highlight: "auto",
+        },
+        c: {
+          entry: ["configurable"],
+          highlight: "auto",
+        },
+        escape: ["done"],
+        p: {
+          entry: ["pretty shortcuts", "pretty"],
+          highlight: "auto",
+        },
+        v: {
+          entry: ["advanced"],
+          highlight: "auto",
         },
       },
       right: [
-        { entry: ["basic"] },
+        {
+          entry: ["configurable"],
+          helpText: `Build your own example menu`,
+        },
+        {
+          entry: ["advanced workflow", "advanced"],
+          helpText: `A complex usage example. Utilizes font awesome icons`,
+        },
         {
           entry: ["async callbacks", "async"],
           helpText: [
@@ -81,19 +139,111 @@ export class MenuSampler {
             `Return response messages to console`,
           ].join(`\n`),
         },
+        {
+          entry: ["pretty shortcuts", "pretty"],
+          helpText: "Showing different methods of coloring keyboard shortcuts",
+        },
       ],
+      value,
     });
     switch (action) {
-      case "basic":
-        await this.basic();
+      case "done":
         return;
+      case "configurable":
+        await this.configurable();
+        return await this.exec(action);
       case "async":
         await this.async();
-        return;
+        return await this.exec(action);
+      case "pretty":
+        await this.prettyShortcuts();
+        return await this.exec(action);
+      case "advanced":
+        this.storedValues = [];
+        this.hiddenTypes = [];
+        await this.advanced();
+        return await this.exec(action);
     }
   }
 
+  private async advanced(value?: AdvancedMenuResult): Promise<void> {
+    this.application.setHeader("Advanced Example");
+    const left = PRE_GENERATED_MENU.filter(
+      ({ type }) => !this.hiddenTypes.includes(type),
+    );
+    const keyMapCallback: MainMenuCB<AdvancedMenuResult> = (key, [, value]) => {
+      switch (key) {
+        case "turn_on":
+          selectedValue = value;
+          this.hiddenTypes = [];
+          return true;
+        case "turn_off":
+          selectedValue = value;
+          this.hiddenTypes = is.unique(
+            PRE_GENERATED_MENU.map(({ type }) => type),
+          );
+          return true;
+        case "toggle":
+          if (is.string(value)) {
+            return `Can only toggle on type entries`;
+          }
+          selectedValue = value;
+          this.hiddenTypes = this.hiddenTypes.includes(value.type)
+            ? this.hiddenTypes.filter(i => i !== value.type)
+            : [...this.hiddenTypes, value.type];
+          return true;
+      }
+      return true;
+    };
+    const types = is.unique(PRE_GENERATED_MENU.map(({ type }) => type)).map(
+      type =>
+        ({
+          entry: [type, { type }],
+          icon: this.hiddenTypes.includes(type)
+            ? chalk.red(FontAwesomeIcons.toggle_off)
+            : chalk.green(FontAwesomeIcons.toggle_on),
+          type: "Types",
+        } as MainMenuEntry<{ type: string }>),
+    );
+    if (is.object(value)) {
+      const type = value.type;
+      value = GV(types.find(i => GV(i).type === type));
+    }
+    let selectedValue: AdvancedMenuResult;
+    const result = await this.prompt.menu<string | { type: string }>({
+      keyMap: {
+        "[": [chalk.blue.dim("turn off all types"), "turn_off"],
+        "]": [chalk.blue.dim("turn on all types"), "turn_on"],
+        escape: ["done"],
+        t: [chalk.blue.dim("toggle type visibility"), "toggle"],
+      },
+      keyMapCallback,
+      left,
+      leftHeader: chalk`Options ({gray ${left.length}} / {gray ${PRE_GENERATED_MENU.length}})`,
+      right: [...types],
+      rightHeader: "Filters",
+      value,
+    });
+
+    switch (result) {
+      case "turn_on":
+      case "turn_off":
+      case "toggle":
+        return await this.advanced(selectedValue);
+      case "done":
+        return;
+    }
+    if (is.object(result)) {
+      this.hiddenTypes = this.hiddenTypes.includes(result.type)
+        ? this.hiddenTypes.filter(i => i !== result.type)
+        : [...this.hiddenTypes, result.type];
+      return await this.advanced(result);
+    }
+    await this.prompt.acknowledge();
+  }
+
   private async async(): Promise<void> {
+    this.application.setHeader("Async Example");
     const result = await this.prompt.menu<tAsyncExample>({
       condensed: true,
       headerMessage: [
@@ -139,7 +289,8 @@ export class MenuSampler {
     await this.prompt.acknowledge();
   }
 
-  private async basic(): Promise<void> {
+  private async configurable(): Promise<void> {
+    this.application.setHeader("Configurable example");
     const cancel = Date.now();
     const menuOptions = await this.prompt.objectBuilder<
       tMenuOptions,
@@ -155,58 +306,75 @@ export class MenuSampler {
       current: this.menuOptions,
       elements: [
         {
+          helpText: [
+            chalk`Hide some of the unnecessary keyboard shortcuts ({gray home}/{gray end}/{gray pageUp}/{gray pageDown})`,
+            "Shortcuts will still work, for aesthetic purposes",
+          ].join("\n"),
           name: "Condensed",
           path: "condensed",
           type: "boolean",
         },
         {
+          helpText: "Remove the tab to toggle search option",
           name: "Hide Search",
           path: "hideSearch",
           type: "boolean",
         },
         {
+          helpText: [
+            "Prevent menu from printing warnings when a list is empty.",
+            "Most useful when using menu as a keyboard shortcut only mode (no left/right entries).",
+          ].join("\n"),
           name: "Key Only",
           path: "keyOnly",
           type: "boolean",
         },
         {
+          helpText: "Display extra text at the top of the menu.",
           name: "Header Message",
           path: "headerMessage",
           type: "string",
         },
         {
+          helpText: "Column header for left column",
           name: "Left Header",
           path: "leftHeader",
           type: "string",
         },
         {
+          helpText: "Column header for right column",
           name: "Right Header",
           path: "rightHeader",
           type: "string",
         },
         {
+          helpText: "Allow showing of help text with this menu (default true)",
           name: "Show Help",
           path: "showHelp",
           type: "boolean",
         },
         {
+          helpText: "Display column headers",
           name: "Show Headers",
           path: "showHeaders",
           type: "boolean",
         },
         {
+          helpText: "Generate fake data",
           name: chalk.cyan("Options left"),
           options: Object.values(FakerSources).map(i => ({ entry: [i] })),
           path: "optionsLeft",
           type: "enum",
         },
         {
+          helpText: "Generate fake data",
           name: chalk.cyan("Options right"),
           options: Object.values(FakerSources).map(i => ({ entry: [i] })),
           path: "optionsRight",
           type: "enum",
         },
         {
+          helpText: "Generate fake data",
           name: chalk.cyan("Generate Options qty"),
           path: "generateCount",
           type: "number",
@@ -289,5 +457,44 @@ export class MenuSampler {
       ]),
       type: TitleCase(type),
     };
+  }
+
+  private async prettyShortcuts(): Promise<void> {
+    this.application.setHeader("Pretty shortcuts");
+    const result = await this.prompt.menu({
+      condensed: true,
+      hideSearch: true,
+      keyMap: {
+        a: {
+          entry: ["auto"],
+          highlight: "auto",
+        },
+        c: [chalk.green("i'm green!"), "green"],
+        n: ["nothing special"],
+        q: {
+          entry: ["specific colors b", "specificB"],
+          highlight: {
+            normal: chalk.gray,
+            valueMatch: chalk.magenta.bold,
+          },
+        },
+        r: {
+          entry: ["specific colors a", "specificA"],
+          highlight: {
+            normal: chalk.blue.bold,
+            valueMatch: chalk.red.bold,
+          },
+        },
+      },
+      right: [
+        { entry: ["auto"] },
+        { entry: ["specific colors a", "specificA"] },
+        { entry: ["specific colors b", "specificB"] },
+        { entry: ["nothing special"] },
+        { entry: [chalk.green("i'm green!"), "green"] },
+      ],
+    });
+    this.screen.printLine(result);
+    await this.prompt.acknowledge();
   }
 }
