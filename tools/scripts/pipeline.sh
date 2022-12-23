776956b813849d7dec81be620c9dbc2dd6e7ee06
mp3three <git@mp3three.dev>
2022-12-22 20:43:21 -0600

    build

--- tools/scripts/pipeline.sh
+++ tools/scripts/pipeline.sh
@@ -3,4 +3,6 @@ npx nx run-many --target=test --all || exit 1
 npx nx run-many --target=build --all --configuration=production || exit 1
 node tools/scripts/pipeline.js
 npx nx run-many --target=build --all --configuration=production || exit 1
-npx nx run-many --target=publish --all
+npx nx run-many --target=publish --all || exit 1
+git commit -a -m "build"
+git push
