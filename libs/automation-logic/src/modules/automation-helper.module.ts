256e2bfb56eaacb2a604b258d647bd833da03b03
mp3three <git@mp3three.dev>
2023-01-08 18:45:56 -0600

    build

--- libs/automation-logic/src/modules/automation-helper.module.ts
+++ libs/automation-logic/src/modules/automation-helper.module.ts
@@ -1,13 +1,13 @@
 import { DiscoveryModule } from "@nestjs/core";
 import { LibraryModule, RegisterCache } from "@steggy/boilerplate";
 
-import { EntityToolsService, QuickActionService } from "../services";
+import { QuickActionService } from "../services";
 
 @LibraryModule({
   configuration: {},
   exports: [QuickActionService],
   imports: [DiscoveryModule, RegisterCache()],
   library: Symbol("automation-logic"),
-  providers: [EntityToolsService, QuickActionService],
+  providers: [QuickActionService],
 })
 export class AutomationHelperModule {}
