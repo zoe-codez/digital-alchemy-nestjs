195b0c2ca185fb95f7a02d7a59c57ba16b6c4596
mp3three <git@mp3three.dev>
2023-01-10 09:37:03 -0600

    pipeline tweak

--- tools/scripts/pipeline.sh
+++ tools/scripts/pipeline.sh
@@ -3,6 +3,8 @@ npx nx run-many --target=test --all || exit 1
 npx nx run-many --target=build --all --configuration=production || exit 1
 node tools/scripts/pipeline.js
 npx nx run-many --target=build --all --configuration=production || exit 1
-npx nx run-many --target=publish --all || exit 1
+# Sometimes the first publish will fail to pick up `~/.npmrc`... because it hates me
+# Second attempt will run from cache for successfully published, only actually re-running failed
+npx nx run-many --target=publish --all || npx nx run-many --target=publish --all || exit 1
 git commit -a -m "build"
 git push
