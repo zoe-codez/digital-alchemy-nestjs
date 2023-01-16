d8a80602b96db2dfa17dce36baff141531dfbdf8
mp3three <git@mp3three.dev>
2023-01-16 09:44:18 -0600

    Adding notes to pipeline

--- tools/scripts/pipeline.sh
+++ tools/scripts/pipeline.sh
@@ -1,10 +1,30 @@
 #!/bin/bash
+# Run all tests, bail out if any fail
 npx nx run-many --target=test --all || exit 1
+
+# Verify everything correctly builds (before bumping versions)
 npx nx run-many --target=build --all --configuration=production || exit 1
+
+# Bump package.json versions
 node tools/scripts/pipeline.js
+
+# Build again (transfer package updates to `dist`)
 npx nx run-many --target=build --all --configuration=production || exit 1
-# Sometimes the first publish will fail to pick up `~/.npmrc`... because it hates me
-# Second attempt will run from cache for successfully published, only actually re-running failed
+
+# Publish packages out
+# ---
+# For some reason, the publish will sometimes inexplicably fail:
+# - phantom build error
+# - stuff related to cannot find `~/.npmrc` (most common)
+# - magic swamp gas
+#
+# If any of the builds fail, then retry
+# NX will run from cache for successful ones
+# With only 1-2 failed, they shouldn't re-fail a 2nd time. Haven't observed it happen yet
 npx nx run-many --target=publish --all || npx nx run-many --target=publish --all || exit 1
+
+# Commit and push
+# ---
+# Want commits with only package updates to appear with the "build" commit
 git commit -a -m "build"
 git push
