41dc269efd6ac224b3515f933c0009c79f0accfd
mp3three <git@mp3three.dev>
2023-02-26 10:46:51 -0600

    Starting on reference install

---
+++ apps/examples/README.md
@@ -0,0 +1,17 @@
+# Examples
+
+This folder contains more practical examples of how to use the libraries.
+These examples are not published, but are runnable by cloning this repo and running locally.
+
+## `Home Assistant` based
+
+A reference docker install of Home Assistant is provided for use with these examples.
+It is possible to use all the code with an existing install, but there may be additional cleanup work involved.
+
+### Reference install
+
+> username: steggy
+> password: nom nom
+
+The reference install is a barely configured Home Assistant install, set up on a non-standard port as to not conflict with any other installs.
+It
--- libs/automation-logic/src/services/scene-controller.service.ts
+++ libs/automation-logic/src/services/scene-controller.service.ts
@@ -2,15 +2,22 @@ import { Inject, Injectable } from "@nestjs/common";
 import { AutoLogService } from "@steggy/boilerplate";
 import {
   iCallService,
+  Icon,
   InjectCallProxy,
+  PICK_GENERATED_ENTITY,
+  PUSH_PROXY,
   PushEntityService,
   PushProxyService,
 } from "@steggy/home-assistant";
+import { each } from "@steggy/utilities";
 
 import {
+  ALL_ROOM_NAMES,
   AUTOMATION_LOGIC_MODULE_CONFIGURATION,
   AutomationLogicModuleConfiguration,
 } from "../types";
+
+const icon: Icon = undefined;
 
 @Injectable()
 export class SceneControllerService {
@@ -24,16 +31,23 @@ export class SceneControllerService {
     private readonly pushEntity: PushEntityService,
   ) {}
 
-  protected onModuleInit(): void {
-    //
+  private readonly currentScenes = new Map<
+    ALL_ROOM_NAMES,
+    PUSH_PROXY<PICK_GENERATED_ENTITY<"sensor">>
+  >();
+
+  protected async onModuleInit(): Promise<void> {
+    await this.buildCurrentSceneSensors();
   }
 
-  private buildCurrentSceneSensors(): void {
+  private async buildCurrentSceneSensors(): Promise<void> {
     const rooms = Object.keys(this.configuration.room_configuration ?? {});
-    rooms.forEach(room => {
-      this.pushEntity.insert(`sensor.room_${room}_current_scene`, {
-        // ""
-      });
+    await each(rooms, async (room: ALL_ROOM_NAMES) => {
+      this.logger.info(`[%s] building sensors`, room);
+      const id =
+        `sensor.room_${room}_current_scene` as PICK_GENERATED_ENTITY<"sensor">;
+      this.pushEntity.insert(id, { icon });
+      this.currentScenes.set(room, await this.pushProxy.createPushProxy(id));
     });
   }
 }
