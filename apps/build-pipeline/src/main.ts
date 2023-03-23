dd1e2a6b45b7c181a1792a432d94b6794806d075
zoe-codez <git@zoe-codez.xyz>
2023-03-23 12:56:26 -0500

    working commit

--- apps/build-pipeline/src/main.ts
+++ apps/build-pipeline/src/main.ts
@@ -1,27 +1,47 @@
-/* eslint-disable sonarjs/no-duplicate-string */
 import { QuickScript } from "@digital-alchemy/boilerplate";
 import JSON from "comment-json";
 import dayjs from "dayjs";
+import execa from "execa";
 import { readFileSync, writeFileSync } from "fs";
 import { join } from "path";
 import { inc } from "semver";
+import { PackageJson } from "type-fest";
+
+const root = "package.json";
 
 @QuickScript()
 export class BuildPipelineService {
-  public exec(): void {
-    const rootPackage = JSON.parse(
-      readFileSync("package.json", "utf8"),
-    ) as unknown as {
-      version: string;
-    };
+  public async exec(): Promise<void> {
+    // ? Bump root package.json version
+    // * Load root package
+    const rootPackage = JSON.parse(readFileSync(root, "utf8")) as PackageJson;
+
+    // * Bump version
     const prefix = dayjs().format("YY.ww").replace(".0", ".");
     rootPackage.version = rootPackage.version.startsWith(prefix)
       ? inc(rootPackage.version, "patch")
       : `${prefix}.0`;
-    writeFileSync(
-      "package.json",
-      JSON.stringify(rootPackage, undefined, "  ") + `\n`,
-    );
+
+    // * Write back
+    writeFileSync(root, JSON.stringify(rootPackage, undefined, "  ") + `\n`);
+
+    // ? Match to library versions
+    // * Seek out all project files
+    const projectFiles = await execa("find", [
+      ".",
+      // eslint-disable-next-line spellcheck/spell-checker
+      "-iname",
+      `*project.json`,
+      "-not",
+      "-path",
+      "./node_modules",
+    ]);
+    // * Iterate over list, updating the package files in their base dirs
+    projectFiles.stdout.split("\n").forEach(project => {
+      const content = JSON.parse(
+        readFileSync(project.replace("project.json", "package.json"), "utf8"),
+      );
+    });
 
     const workspace = JSON.parse(
       readFileSync("workspace.json", "utf8"),
@@ -30,13 +50,13 @@ export class BuildPipelineService {
     };
     Object.values(workspace.projects).forEach(base => {
       const packageJSON = JSON.parse(
-        readFileSync(join(base, "package.json"), "utf8"),
+        readFileSync(join(base, root), "utf8"),
       ) as unknown as {
         version: string;
       };
       packageJSON.version = rootPackage.version;
       writeFileSync(
-        join(base, "package.json"),
+        join(base, root),
         JSON.stringify(packageJSON, undefined, "  ") + `\n`,
       );
     });
