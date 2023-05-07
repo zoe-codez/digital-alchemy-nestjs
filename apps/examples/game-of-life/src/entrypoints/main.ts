6414b70ebf79d52095d00810b7c48e487a34d474
Zoe <git@zoe-codez.xyz>
2023-05-06 22:41:23 -0500

    missing file

---
+++ apps/examples/game-of-life/src/entrypoints/main.ts
@@ -0,0 +1,31 @@
+import {
+  LIB_BOILERPLATE,
+  LOG_LEVEL,
+  QuickScript,
+} from "@digital-alchemy/boilerplate";
+import { RGBMatrixModule } from "@digital-alchemy/rgb-matrix";
+import { ApplicationManagerService, TTYModule } from "@digital-alchemy/tty";
+
+import { ConwayService, GameOfLifeComponentService } from "../services";
+
+@QuickScript({
+  application: "game-of-life",
+  bootstrap: {
+    application: {
+      config: {
+        libs: {
+          [LIB_BOILERPLATE]: { [LOG_LEVEL]: "silent" },
+        },
+      },
+    },
+  },
+  imports: [RGBMatrixModule, TTYModule],
+  providers: [ConwayService, GameOfLifeComponentService],
+})
+export class GameOfLifeCLI {
+  constructor(private readonly application: ApplicationManagerService) {}
+
+  public async exec(): Promise<void> {
+    await this.application.activateComponent("game-of-life");
+  }
+}
