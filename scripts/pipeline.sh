0eaadc5eb300e2cece8c45766ffe4a34a8f046ab
Zoe <git@zoe-codez.xyz>
2023-05-09 11:46:06 -0500

    migrate pt 1

--- scripts/pipeline.sh
+++ scripts/pipeline.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
+
+
 # Run all tests, bail out if any fail
 npx nx run-many --target=test --all || exit 1
 
 # Verify everything correctly builds (before bumping versions)
 npx nx run-many --target=build --all --configuration=production || exit 1
+exit
 
-# Bump package.json versions
-./bin/build-pipeline
+# # Bump package.json versions
+# ./bin/build-pipeline
 
-# Build again (transfer package updates to `dist`)
-npx nx run-many --target=build --all --configuration=production || exit 1
+# # Build again (transfer package updates to `dist`)
+# npx nx run-many --target=build --all --configuration=production || exit 1
 
-# Publish packages out
-# ---
-# For some reason, the publish will sometimes inexplicably fail:
-# - phantom build error
-# - stuff related to cannot find `~/.npmrc` (most common)
-# - magic swamp gas
-#
-# If any of the builds fail, then retry
-# NX will run from cache for successful ones
-# With only 1-2 failed, they shouldn't re-fail a 2nd time. Haven't observed it happen yet
+# exit
+
+# # Publish packages out
+# # ---
+# # For some reason, the publish will sometimes inexplicably fail:
+# # - phantom build error
+# # - stuff related to cannot find `~/.npmrc` (most common)
+# # - magic swamp gas
+# #
+# # If any of the builds fail, then retry
+# # NX will run from cache for successful ones
+# # With only 1-2 failed, they shouldn't re-fail a 2nd time. Haven't observed it happen yet
 # npx nx run-many --target=publish --all || npx nx run-many --target=publish --all || exit 1
 
 # # Commit and push
